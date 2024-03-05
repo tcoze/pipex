a2134a4d4d9de963e49ce593bc9c4be7ad8c7d2b
Timothe Coze <tcoze@z4r2p4.42lyon.fr>
2024-03-05 16:11:34 +0100

    a voir

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tcoze <tcoze@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 10:27:43 by tcoze             #+#    #+#             */
-/*   Updated: 2024/03/02 10:27:43 by tcoze            ###   ########.fr       */
+/*   Updated: 2024/03/05 13:43:06 by tcoze            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,20 @@ static void print_fd(void)
 int	pipex(struct s_cmd *cmd, char **envp)
 {
 	pid_t	pid1;
+	pid_t	pid2;
 	int		status;
 	int		pfd[2];
 
-
-	if ((cmd->s_path != NULL || cmd->f_path) && cmd->f2 && pipe(pfd) == -1)
+	pfd[0] = 0;
+	pfd[1] = 0;
+	pid1 = 0;
+	pid2 = 0;
+	if ((cmd->s_path != NULL || cmd->f_path != NULL) && pipe(pfd) == -1)
 	{
 		if (cmd->f1 >= 0)
 			close(cmd->f1);
 		if (cmd->f2 >= 0)
-			close (cmd->f2);
+			close(cmd->f2);
 		return (-1);
 	}
 	if (cmd->f_path != NULL && cmd->f1 >= 0)
@@ -53,21 +57,32 @@ int	pipex(struct s_cmd *cmd, char **envp)
 		if (pid1 == 0)
 			if (child_process(cmd, envp, pfd) == -1)
 				return (-1);
-		close(cmd->f1);
-	}
-	if (cmd->s_path != NULL && cmd->f2 >= 0)
-	{
-		if (parent_process (cmd, envp, pfd) == -1)
-			return (-1);
-		close(cmd->f2);
-		if (cmd->first != NULL)
-			while (errno != ECHILD)
-				waitpid (pid1, &status, 0);
 	}
 	if (cmd->f1 >= 0)
 		close(cmd->f1);
+	if (pfd[1] >= 0)
+		close(pfd[1]);
+	if (cmd->s_path != NULL && cmd->f2 >= 0)
+	{
+		pid2 = fork ();
+		if (pid2 == -1)
+		{
+			if (cmd->f2 >= 0)
+				close(cmd->f2);
+			return (-1);
+		}
+		if (pid2 == 0)
+			if (parent_process(cmd, envp, pfd) == -1)
+				return (-1);
+	}
+	if (pid1 != 0)
+		waitpid(pid1, &status, 0);
+	if (pid2 != 0)
+		waitpid(pid2, &status, 0);
 	if (cmd->f2 >= 0)
 		close(cmd->f2);
+	if (pfd[0] >= 0)
+		close(pfd[0]);
 	return (0);
 }
 
@@ -82,6 +97,12 @@ int	main(int argc, char *argv[], char **envp)
 	if (argc != 5)
 		return (ft_printf (2, "Put only 5 arguments\n"), -1);
 	cmd.f2 = open (argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);  // FILE 2 OPEN ICI
+	if (envp == NULL)
+	{
+		if (cmd.f2 >= 0)
+			return (close(cmd.f2), -1);
+		return (-1);
+	}
 	cmd.f1 = open (argv[1], O_RDONLY);  // FILE 1 OPEN ICI
 	if (cmd.f1 < 0)
 		ft_printf (2, "%s: No such file or directory\n", argv[1]);
@@ -91,15 +112,7 @@ int	main(int argc, char *argv[], char **envp)
 			close(cmd.f1);
 		return (close(cmd.f2), -1);
 	}
-	if (cmd.second == NULL)
-	{
-		if (cmd.f1 >= 0)
-			close(cmd.f1);
-		if (cmd.f2 >= 0)
-			close(cmd.f2);
-		return (clear_struct (&cmd), 0);
-	}
 	if (pipex(&cmd, envp) == -1)
-		return (clear_struct(&cmd), -1);
+		return (clear_struct (&cmd), -1);
 	return (clear_struct(&cmd), 0);
 }
--- src/process.c
+++ src/process.c
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   process.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: tcoze <tcoze@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:44:02 by tcoze             #+#    #+#             */
+/*   Updated: 2024/03/05 13:44:02 by tcoze            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: logname <logname@student.1337.ma.          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 18:10:49 by logname           #+#    #+#             */
@@ -28,57 +40,51 @@
 
 int	child_process(struct s_cmd *cmd, char **envp, int *pfd)
 {
-	if (cmd->f1 >= 0 && cmd->f_path && cmd->first)
+	if (cmd->f_path && cmd->f1 >= 0)
 	{
-		if (dup2(cmd->f1, STDIN_FILENO) < 0)
-			return (close(cmd->f1), -1);
-		if (dup2(pfd[STDOUT_FILENO], STDOUT_FILENO) < 0)
-			return (close(cmd->f1), -1);
-		if (close(pfd[STDIN_FILENO]) == -1)
-			return (close(pfd[STDOUT_FILENO]), close (cmd->f1), -1);
-		if (close(pfd[STDOUT_FILENO]) == -1)
+		if (dup2 (cmd->f1, STDIN_FILENO) < 0)
 			return (close (cmd->f1), -1);
-		if (close(cmd->f1) == -1)
+		if (dup2 (pfd[1], STDOUT_FILENO) < 0)
+			return (close (cmd->f1), -1);
+		if (close (pfd[0]) == -1)
+			return (close (pfd[1]), close (cmd->f1), -1);
+		if (close (pfd[1]) == -1)
+			return (close (cmd->f1), -1);
+		if (close (cmd->f1) == -1)
 			return (-1);
-		if (close(cmd->f2) >= 0)
-			close(cmd->f2);
+		if (close (cmd->f2) >= 0)
+			close (cmd->f2);
 		if (execve (cmd->f_path, cmd->first, envp) == -1)
-			return (close (0), close (1), -1);
+			return (close (0), close (1), dprintf (2, "Command not found : %s\n", cmd->first[0]), -1);
 	}
-	else
-	{
-		if (close(cmd->f1) == -1)
-			return (close(cmd->f2), -1);
-		if (close(cmd->f2) >= 0)
-			close(cmd->f2);
-	}
+	if (cmd->f1 >= 0)
+		close(cmd->f1);
+	if (cmd->f2 >= 0)
+		close(cmd->f2);
 	return (0);
 }
 
 int	parent_process(struct s_cmd *cmd, char **envp, int *pfd)
 {
-	if (cmd->s_path && cmd->second)
+	if (cmd->s_path && cmd->f2 >= 0)
 	{
-		if (cmd->f2 >= 0)
-		{
-			if (dup2 (cmd->f2, STDOUT_FILENO) < 0)
-				return (close (cmd->f2), -1);
-			if (dup2 (pfd[STDIN_FILENO], STDIN_FILENO) < 0)
-				return (close (cmd->f2), -1);
-			if (close (pfd[STDOUT_FILENO]) == -1)
-				return (close (pfd[STDIN_FILENO]), close (cmd->f2), -1);
-			if (close (pfd[STDIN_FILENO]) == -1)
-				return (close (cmd->f2), -1);
-			if (close (cmd->f2) == -1)
-				return (-1);
-		}
-//		print_fd();
-//		dprintf(2, "f2 : %d\n", cmd->f2);
+		if (dup2(cmd->f2, STDOUT_FILENO) < 0)
+			return (close (cmd->f2), -1);
+		if (dup2(pfd[0], STDIN_FILENO) < 0)
+			return (close (cmd->f2), -1);
+		//if (close(pfd[1]) == -1)
+		//	return (close (pfd[0]), close (cmd->f2), -1);
+		if (close(pfd[0]) == -1)
+			return (close (cmd->f2), -1);
+		if (close(cmd->f2) == -1)
+			return (-1);
+		if (cmd->f1 >= 0)
+			close(cmd->f1);
 		if (execve(cmd->s_path, cmd->second, envp) == -1)
-			return (close (0), close (1), -1);
+			return (close(0), close(1), close (2), dprintf (2, "Command not found : %s\n", cmd->first[0]), -1);
 	}
 	else
-		if (close(cmd->f2) >= 0)
+		if (cmd->f2 >= 0)
 			close(cmd->f2);
 	return (0);
 }
